cabb95c04e568153222fd804e05c5b557d9775b1
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-22 12:17:04 -0500

    Add ship movement.
    Get ship moving in scene and clamp it to keep in on screen.

---
+++ Assets/Player.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class Player : MonoBehaviour
+{
+    [Tooltip("In ms^-1")][SerializeField] float xSpeed = 4f;
+    [Tooltip("In m")][SerializeField] private float screenEdge = 5f;
+
+    // Update is called once per frame
+    void Update()
+    {
+        var posVec = transform.localPosition;
+        var xThrow = CrossPlatformInputManager.GetAxis("Horizontal");
+        var xOffset = xSpeed * Time.deltaTime * xThrow;
+        var rawXPos = posVec.x + xOffset;
+        var clampedXPos = Mathf.Clamp(rawXPos, -screenEdge, screenEdge);
+        transform.localPosition = new Vector3(clampedXPos, posVec.y, posVec.z);
+    }
+}
