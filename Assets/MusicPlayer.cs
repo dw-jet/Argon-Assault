989078ac90c75c60bb4004e2f8d8b9e40abb744e
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-22 00:01:51 -0500

    Add Standard Assets
    Add standard assets. Add audio and waypoints.

---
+++ Assets/MusicPlayer.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MusicPlayer : MonoBehaviour
+{
+    private void Awake()
+    {
+        DontDestroyOnLoad(gameObject);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Invoke("LoadLevel", 5f);
+    }
+
+    void LoadLevel()
+    {
+        SceneManager.LoadScene(1);
+    }
+}
