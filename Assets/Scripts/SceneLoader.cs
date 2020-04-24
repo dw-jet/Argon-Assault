ee9c770fc2548a9c4fb731fbdfb6c488fae156f6
James Teitsworth <63876458+dw-jet@users.noreply.github.com>
2020-04-24 00:41:30 -0500

    Tidy up project files and seperate concerns
    Just cleaning up and organizing things and seperating the music playing and scene loading into two seperate scripts.

--- Assets/Scripts/SceneLoader.cs
+++ Assets/Scripts/SceneLoader.cs
@@ -1,16 +1,9 @@
-﻿using System;
-using System.Collections;
-using System.Collections.Generic;
-using UnityEngine;
+﻿using UnityEngine;
 using UnityEngine.SceneManagement;
 
-public class MusicPlayer : MonoBehaviour
+public class SceneLoader : MonoBehaviour
 {
-    private void Awake()
-    {
-        DontDestroyOnLoad(gameObject);
-    }
-
+    // Start is called before the first frame update
     // Start is called before the first frame update
     void Start()
     {
